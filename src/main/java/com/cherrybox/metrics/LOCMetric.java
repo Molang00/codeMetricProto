96cb600830b98506a2ad5bb132be218e6f82fc16
byeongsu <bskim00@postech.ac.kr>
2020-02-23 14:40:11 +0900

    first commit

---
+++ src/main/java/com/cherrybox/metrics/LOCMetric.java
@@ -0,0 +1,53 @@
+package com.cherrybox.metrics;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.*;
+
+/**
+ * Calculate the Physical Length of Code (LOC). LOC is one metric of Source
+ * lines of code (SLOC), which is a software metric used to measure the size of
+ * a computer program by counting the number of lines in the text of the
+ * program's source code.
+ */
+public class LOCMetric extends Metric {
+    public LOCMetric(String name) {
+        super(name);
+    }
+
+    /**
+     * Calculate LOC.
+     * 
+     * @param project the project working on.
+     * @param target  the class to be calculated.
+     * @return the value of LOC.
+     */
+    @Override
+    public double calculate(Project project, PsiClass target) {
+        String str = target.getText();
+        if (str == null) {
+            lastResult = 0;
+            return 0;
+        } else {
+            String[] lines = str.split("\r\n|\r|\n");
+            lastResult = lines.length;
+            return lastResult;
+        }
+    }
+
+    @Override
+    public String generateReport() {
+        String rst = "전체 코드의 길이가 ";
+        if (lastResult < 20) {
+            rst += "매우 적고, ";
+        } else if (lastResult < 50) {
+            rst += "적고, ";
+        } else if (lastResult < 150) {
+            rst += "보통이고, ";
+        } else if (lastResult < 300) {
+            rst += "많고, ";
+        } else {
+            rst += "매우 많고, ";
+        }
+        return rst;
+    }
+}
---
+++ src/main/java/com/cherrybox/metricsManager.java
@@ -0,0 +1,41 @@
+package com.cherrybox;
+
+import com.cherrybox.metrics.LOCMetric;
+import com.cherrybox.metrics.Metric;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class metricsManager {
+    private List<Metric> metrics;
+    public metricsManager(){
+        Metric LOC = new LOCMetric("LOC");
+        metrics = Arrays.asList(LOC);
+    }
+
+    public void exitReport() throws IOException {
+        double total = 0;
+        double error = 0;
+        String report = "";
+        PrintWriter pw = new PrintWriter("out.txt");
+        report += "이 코드는 일반적인 코드에 비해 ";
+        for (Metric m : metrics) {
+            String sentance = m.generateReport();
+            report += sentance;
+            total++;
+            if (sentance.contains("매우"))
+                error++;
+        }
+        double errorPercent = error / total;
+        if (errorPercent < 0.25) {
+            report += "유지 및 보수하기 좋은 구조로 되어 있습니다.\n";
+        } else if (errorPercent < 0.75) {
+            report += "평범한 수준으로 개발되어 있습니다.\n";
+        } else {
+            report += "전체적으로 수정이 요구됩니다.\n";
+        }
+        System.out.println(report);
+    }
+}
